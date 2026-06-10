bf79438647042f7de5267cb78d46210256f3c465
henryppi <green.spam@web.de>
2026-06-10 12:32:49 +0200

    update

---
+++ run_create_propeller_cad.py
@@ -0,0 +1,18 @@
+# points = np.array(points)
+# points = np.unique(points,axis=0)
+
+# flip direction
+# points[:,0] *= -1
+
+# for cubic in cubics:
+#     ax.add_patch(cubic)
+# ax.plot(points[:,0],points[:,1],'.-k')
+
+# rad = np.array([72.5,87.0,101.5,116.0,145.0,174.0,203.0,232.0,261.0,275.5,282.8,287.1])
+# fig, ax = plt.subplots()
+# for i in range(12):
+#     sec = sections[i]
+#     ax.plot(sec[:,0],sec[:,1]+rad[i])
+
+# plt.axis('equal')
+# plt.show()
--- run_extract_polyline_from_svg_prop.py
+++ run_extract_polyline_from_svg_prop.py
@@ -31,19 +31,8 @@ for j in range(n_paths):
                     (np.imag(segment.end),np.real(segment.end))
                 ]
                 profile.append(cp)
-                # cp_path = [mpath.Path.MOVETO,mpath.Path.CURVE4,mpath.Path.CURVE4,mpath.Path.CURVE4]
-                # bezier_path = mpath.Path(cp,cp_path)
-                # cubic = mpatches.PathPatch(bezier_path,facecolor='none',edgecolor='b',lw=2)
-                # cubics.append(cubic)
-
-                # interpolated_path = bezier_path.interpolated(steps=10)
-                # curve_points = interpolated_path.vertices
-                # print(j)
-                # print(curve_points)
-                # ax.add_patch(patch)
         if len(profile)>0:
             profiles.append(profile)
-
 
 t = np.linspace(0, 1, 91)
 sections = []
@@ -58,10 +47,8 @@ for i in range(nprof):
         nline = len(seg)
 
         if nline==2:
-            #lin
             add_seg = np.array(seg).T
         elif nline==4:
-            #cubic
             add_seg = np.array([cubic_bezier(ti, np.array(seg[0]), np.array(seg[1]), np.array(seg[2]), np.array(seg[3])) for ti in t])
         else:
             add_seg = np.zeros([0,2],float)
@@ -75,26 +62,9 @@ for i in range(nprof):
 
     sections.append(section)
 
-
 sections = sections[::-1]
-rad = np.array([72.5,87.0,101.5,116.0,145.0,174.0,203.0,232.0,261.0,275.5,282.8,287.1])
-fig, ax = plt.subplots()
+
 for i in range(12):
-    sec = sections[i]
-    ax.plot(sec[:,0],sec[:,1]+rad[i])
-# points = np.array(points)
-# points = np.unique(points,axis=0)
-
-# flip direction
-# points[:,0] *= -1
-
-# for cubic in cubics:
-#     ax.add_patch(cubic)
-# ax.plot(points[:,0],points[:,1],'.-k')
-
-fn = './data_files/section_points_propeller.dat'
-fmt='%.18e'
-# np.savetxt(fn,points,fmt=fmt,delimiter='\t')
-
-plt.axis('equal')
-plt.show()
+    fn = './data_files/sections_propeller_sec{:02d}.dat'.format(i)
+    fmt='%.18e'
+    np.savetxt(fn,sections[i],fmt=fmt,delimiter='\t')
